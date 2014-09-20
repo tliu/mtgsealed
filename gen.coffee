29ce3dedef55df819329f79d28ac393cbc9446b6
= <tmkliu@gmail.com>
2014-09-20 15:27:33 -0700

    yey

--- gen.coffee
+++ gen.coffee
@@ -9,15 +9,32 @@ $ ->
                 for s in data
                     p = s.split(":")
                     id = p[0]
-                    @c = p[1]
-                    console.log "out: " + @c
-                    add_card = (data) =>
-                        console.log "in: " + @c
+                    c = p[1]
+                    console.log "out: " + id + ", " + c
+                    add_card = do(c) -> (data) =>
                         console.log data
+                        data = data.replace(/^\s+|\s+$/g, "").replace(/]/g, "").replace(/"/g,"").split(",")
+                        data[3] = data[3][1]
+                        console.log "card-block-#{data[3]}"
+                        if data[3] is "W"
+                            color = "#F0F5B8"
+                        if data[3] is "U"
+                            color = "#2aa9f7"
+                        if data[3] is "B"
+                            color = "#555555"
+                        if data[3] is "R"
+                            color = "#fc3f3f"
+                        if data[3] is "G"
+                            color = "#17bd45"
+                        if data[3] is "M"
+                            color = "#ffd700"
+                        if data[3] is "L"
+                            color = "#4af460"
+                        if data[3] is "A"
+                            color = "#663300"
+                        $("#card-block-#{data[3]}").append("<div class=\"card\" style=\"margin-top: 6px;border: 1px solid #555; border-radius: 5px; padding: 4px; background-color: #{color};\">
+                            <span style=\"font-size: 18px; margin-right: 6px\">#{c}</span> <b>#{data[1]}</b>
+                        </div>")
+
                     $.get "http://127.0.0.1:5000/cards/id/#{id}", add_card
-                
-
-
-
-
 
--- gen.js
+++ gen.js
@@ -11,21 +11,51 @@
       if (e.which === 13) {
         count = $("#num-boosters").val();
         return $.get("http://127.0.0.1:5000/boosters?count=" + count, function(data) {
-          var add_card, id, p, s, _i, _len, _results;
+          var add_card, c, id, p, s, _i, _len, _results;
           data = data.replace("{", "").replace("}", "").replace(/"/g, "").split(", ");
           _results = [];
           for (_i = 0, _len = data.length; _i < _len; _i++) {
             s = data[_i];
             p = s.split(":");
             id = p[0];
-            this.c = p[1];
-            console.log("out: " + this.c);
-            add_card = (function(_this) {
-              return function(data) {
-                console.log("in: " + _this.c);
-                return console.log(data);
-              };
-            })(this);
+            c = p[1];
+            console.log("out: " + id + ", " + c);
+            add_card = (function(c) {
+              return (function(_this) {
+                return function(data) {
+                  var color;
+                  console.log(data);
+                  data = data.replace(/^\s+|\s+$/g, "").replace(/]/g, "").replace(/"/g, "").split(",");
+                  data[3] = data[3][1];
+                  console.log("card-block-" + data[3]);
+                  if (data[3] === "W") {
+                    color = "#F0F5B8";
+                  }
+                  if (data[3] === "U") {
+                    color = "#2aa9f7";
+                  }
+                  if (data[3] === "B") {
+                    color = "#555555";
+                  }
+                  if (data[3] === "R") {
+                    color = "#fc3f3f";
+                  }
+                  if (data[3] === "G") {
+                    color = "#17bd45";
+                  }
+                  if (data[3] === "M") {
+                    color = "#ffd700";
+                  }
+                  if (data[3] === "L") {
+                    color = "#4af460";
+                  }
+                  if (data[3] === "A") {
+                    color = "#663300";
+                  }
+                  return $("#card-block-" + data[3]).append("<div class=\"card\" style=\"margin-top: 6px;border: 1px solid #555; border-radius: 5px; padding: 4px; background-color: " + color + ";\"> <span style=\"font-size: 18px; margin-right: 6px\">" + c + "</span> <b>" + data[1] + "</b> </div>");
+                };
+              })(this);
+            })(c);
             _results.push($.get("http://127.0.0.1:5000/cards/id/" + id, add_card));
           }
           return _results;
